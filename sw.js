240f2548724919d4c30b531eab47dd1fca87d5ce
pkimes30-rgb <pkimes30@gmail.com>
2026-09-18 04:47:39 -0500

    Publish AnimeHub web app

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="animehub-v2";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./icons/icon.svg"]))));self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
